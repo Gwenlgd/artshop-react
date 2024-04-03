8139ea76bb1ae856634935140acdcd8a7069501e
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-04-03 14:57:43 +0200

    Add: total price to cart

--- src/pages/ShoppingCart/ShoppingCart.jsx
+++ src/pages/ShoppingCart/ShoppingCart.jsx
@@ -11,12 +11,19 @@ function ShoppingCart() {
         <h3>No product in your shoppping cart</h3>
       </div>
     );
+
+  const totalPrice = cartItems.reduce(
+    (total, product) => total + parseFloat(product.price) * product.quantity,
+    0
+  );
+
   return (
     <div className="shopping-cart">
       <h2>Shopping Cart</h2>
       <ul>
         {cartItems.map((product) => (
           <li key={product.id}>
+            <br />
             <div className="display-items-cart">
               {product.title} - {product.quantity} -{" "}
               {parseFloat(product.price) * parseInt(product.quantity)}€
@@ -28,8 +35,13 @@ function ShoppingCart() {
                 Remove All
               </button>
             </div>
+            <br />
           </li>
         ))}
+        <br />
+        <div className="cart-total">
+          <strong>Total:</strong> {totalPrice.toFixed(2)}€
+        </div>
       </ul>
     </div>
   );
